d1e492e3a1f9b6f25ae7a9293f272bfe8b069c23
Bumba <Iliyashatami20@gmail.com>
2025-09-30 18:12:35 +0100

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-   <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <h1 className="-font-inter">Hello World</h1>
+      <h1 className="-font-heading"> Whereas recognition of the in</h1>
+      <h1 className="font-inter">Hello World</h1>
+      <h1>Hello World</h1>
+    </>
   );
 }
