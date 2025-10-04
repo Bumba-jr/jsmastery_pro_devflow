d9d8415704581645abd860c25845a0049faa59c5
Bumba <Iliyashatami20@gmail.com>
2025-10-04 21:04:23 +0100

    Installing SchadCN UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export default function Home() {
   return (
     <>
@@ -6,6 +15,18 @@ export default function Home() {
       <h1 className="-font-heading"> Whereas recognition of the in</h1>
       <h1 className="font-inter">Hello World</h1>
       <h1>Hello World</h1>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 }
